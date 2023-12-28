408a5499944f1554b64b9408901a56858c680ee8
akshay <akshay@gmail.com>
2023-12-28 13:21:11 +0530

    added

---
+++ nested.c
@@ -0,0 +1,19 @@
+
+
+/*  3)5 number input 
+>40 
+average 
+3. 45 to 50 – D
+4. 50 to 60 – C
+5. 60 to 80 – B
+6. Above 80 – A 
+otherwise fail   */
+
+ #include <stdio.h>
+ 
+ int main()
+{
+  int 
+  return 0;
+}
+
